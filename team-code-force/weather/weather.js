9cbf135bb2bc105dc659731299b519d66a899bbe
benyoss <benyoss4@gmail.com>
2020-08-19 17:16:03 -0500

    "Added weather directory and file containing weather API call for weather data."

---
+++ team-code-force/weather/weather.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const axios = require('axios');
+
+
+const getWeatherData = (cityName) => {
+    axios({
+        "method":"GET",
+        "url":"https://community-open-weather-map.p.rapidapi.com/forecast",
+        "headers":{
+        "content-type":"application/octet-stream",
+        "x-rapidapi-host":"community-open-weather-map.p.rapidapi.com",
+        "x-rapidapi-key":process.env.WEATHER_API,
+        "useQueryString":true
+        },"params":{
+        "q":cityname,
+        "cnt":"1",
+        }
+        })
+        .then(({ data })=>{
+          return data;
+        })
+        .catch((error)=>{
+          console.log(error)
+        })
+};
+
+module.exports = {
+    getWeatherData,
+};
