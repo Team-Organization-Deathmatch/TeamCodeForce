452affd30b7895178ccaaea510f28f5a1e25a94d
Sebastian Hove <sebastianhove@gmail.com>
2020-08-20 13:32:05 -0500

    "Database issue fixed- ben reployed DB, added FriendRoutes & Invites tabs, successfully rendering"

---
+++ team-code-force/src/components/FriendRoutes.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FriendRoutes = () => {
+  return <div> FRIENDS ROUTES </div>;
+};
+
+export default FriendRoutes;
---
+++ team-code-force/src/components/Invites.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Invites = () => {
+  return <div> TESTING 1 2 3 </div>;
+};
+
+export default Invites;
