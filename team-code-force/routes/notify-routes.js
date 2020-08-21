0164c86df918890473779947f8cc384aa41825d9
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-08-21 18:07:46 -0500

    new server file for route /notify

---
+++ team-code-force/routes/notify-routes.js
@@ -0,0 +1,21 @@
+const notifyRouter = require('express').Router();
+const { Park, UserParkWishList, UserParkHistory, User } = require('../db/index');
+require('dotenv').config();
+const { routeUsers } = require('../db/database')
+// const accountSid = process.env.TWILIO_SID;
+// const authToken = process.env.TWILIO_TOKEN;
+
+// const client = require('twilio')(accountSid, authToken);
+
+notifyRouter.get('/routechange', (req, res) => {
+  console.log('hit the route /test!');
+  routeUsers(null).then((data) => {
+    console.log(data, 'THIS IS YOUR DATA!');
+    res.status(200);
+    res.send(data);
+  })
+})
+
+module.exports = {
+  notifyRouter,
+}
