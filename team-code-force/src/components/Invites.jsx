285434a02d46657e2ebce0f7c7babd865a2c244a
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-08-22 12:30:26 -0500

    updated dummy function

--- team-code-force/src/components/Invites.jsx
+++ team-code-force/src/components/Invites.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import axios from 'axios'
 import { useState } from 'react';
 import { useEffect } from 'react';
-
+const { REACT_APP_SERVER_PORT } = process.env;
 const Invites = () => {
   let [users, setUsers] = useState()
 
   useEffect(() => {
-    axios.get('http://localhost:8000/notify/routechange').then((data) => {
+    axios.get(`http://localhost:${REACT_APP_SERVER_PORT}/notify/routechange`).then((data) => {
       console.log(data);
     })
     .catch(err => console.log(err))
