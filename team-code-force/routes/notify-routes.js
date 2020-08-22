54dd0b3e789f5df2d681a0dc2d8abab6fe497531
MaxwellBoecker <boecker.maxwell@gmail.com>
2020-08-21 19:21:21 -0500

    using twilio in /routechange in notify route

--- team-code-force/routes/notify-routes.js
+++ team-code-force/routes/notify-routes.js
@@ -2,17 +2,19 @@ const notifyRouter = require('express').Router();
 const { Park, UserParkWishList, UserParkHistory, User } = require('../db/index');
 require('dotenv').config();
 const { routeUsersPhone } = require('../db/database')
-// const accountSid = process.env.TWILIO_SID;
-// const authToken = process.env.TWILIO_TOKEN;
-
-// const client = require('twilio')(accountSid, authToken);
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_TOKEN;
+const { sendNotification } = require('../sms/twilio')
+const client = require('twilio')(accountSid, authToken);
 
 notifyRouter.get('/routechange', (req, res) => {
-  console.log('hit the route /test!');
   routeUsersPhone(null).then((data) => {
-    console.log(data, 'THIS IS YOUR DATA!');
-    res.status(200);
-    res.send(data);
+
+    sendNotification('yolo', '+19086123718');
+      console.log('sent');
+      res.status(200);
+      res.send(data);
+    
   })
 })
 
