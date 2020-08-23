4c62445e195bee830cf18d84dd5d05c516632e1a
Sebastian Hove <sebastianhove@gmail.com>
2020-08-22 22:31:28 -0500

    "Added accept & unaccept invite buttons, semi functional"`

--- team-code-force/src/components/Invites.jsx
+++ team-code-force/src/components/Invites.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-const Invites = () => {
-  let [users, setUsers] = useState()
+const Invites = ({ user }) => {
+  let [users, setUsers] = useState();
 
   useEffect(() => {
-    axios.get('http://localhost:8000/notify/routechange').then((data) => {
-      console.log(data);
-    })
-    .catch(err => console.log(err))
-  }, [])
+    axios
+      .get('http://localhost:8000/notify/routechange')
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-  return (<div> 
-    TESTING 1 2 3 
-    </div>);
+  const acceptInvite = () => {
+    console.log(user);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ user }),
+    };
+    fetch('/route/acceptInvite', requestOptions);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          acceptInvite();
+        }}
+      >
+        Accept Invite
+      </button>
+      <button>Decline Invite</button>
+    </div>
+  );
 };
 
 export default Invites;
