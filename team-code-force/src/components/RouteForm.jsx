e1f09c9b9752bb72dae7b5fed3d730cad46d3b78
benyoss <benyoss4@gmail.com>
2020-08-23 08:28:31 -0500

    "Removed calender feature entirely from this file"

--- team-code-force/src/components/RouteForm.jsx
+++ team-code-force/src/components/RouteForm.jsx
@@ -1,22 +1,9 @@
 import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-
-import 'react-datepicker/dist/react-datepicker.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const RouteForm = ({ getpark }) => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [recordDate, setDate] = useState('');
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
-  };
-
-  const handleDate = (date) => {
-    setDate(date);
-  };
-
-  const handleSubmit = (event) => {
-    console.log(event.target.value, 'BOOOM');
   };
 
   const onKeyUp = (event) => {
@@ -36,9 +23,6 @@ const RouteForm = ({ getpark }) => {
           <br />
           <input type="text" placeholder="State Name" value={searchTerm} onChange={handleChange} onKeyUp={onKeyUp} />
         </label>
-        <label>
-          Select a startDate:
-        </label>
         <button
           type="submit"
           onClick={() => {
@@ -49,21 +33,6 @@ const RouteForm = ({ getpark }) => {
           Add To Trip!
         </button>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <DatePicker
-            selected={recordDate}
-            onChange={handleDate}
-            name="startDate"
-            dateFormat="MM-dd-yyyy hh:mm aa"
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={20}
-            timeCaption="time"
-          />
-          <button className="btn btn-primary">Show Date</button>
-        </div>
-      </form>
     </div>
   );
 };
