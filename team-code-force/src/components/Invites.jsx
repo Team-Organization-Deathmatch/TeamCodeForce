28d1dc2338674609eda52a1ef6ea85b4142aeeba
Christopher Stumpe <toph.stumpe@gmail.com>
2020-08-23 20:21:29 -0500

    restoring functionality while backpedaling and fixing invite page

--- team-code-force/src/components/Invites.jsx
+++ team-code-force/src/components/Invites.jsx
@@ -5,28 +5,19 @@ import { useEffect } from 'react';
 const { REACT_APP_SERVER_PORT } = process.env;
 //Seb & Christopher's
 const Invites = ({ user }) => {
-  let [users, setUsers] = useState();
+  // let [users, setUsers] = useState();
 
-  useEffect(() => {
+  // useEffect(() => {
     // axios
     //   .get(`http://localhost:${REACT_APP_SERVER_PORT}/notify/routechange`)
     //   .then((data) => {
     //     console.log(data);
     //   })
     //   .catch((err) => console.log(err));
-  }, []);
+  // }, []);
   //end seb & c's
 // =======
-// const Invites = () => {
-//   let [users, setUsers] = useState()
 
-//   useEffect(() => {
-//     axios.post(`http://localhost:${REACT_APP_SERVER_PORT}/notify/invite`, { number: '+12163859616'}).then((data) => {
-//       console.log(data);
-//     })
-//     .catch(err => console.log(err))
-//   }, [])
-// >>>>>>> master
 
 //seb and c's add's
   const acceptInvite = () => {
@@ -47,6 +38,32 @@ const Invites = ({ user }) => {
     };
     fetch('/route/declineInvite', requestOptions);
   };
+  
+  const checkInvite = () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ user }),
+    };
+    fetch('/route/checkInvite', requestOptions);
+  };
+  const [invites, updateInvites] = useState([ ])
+  // useEffect(() => {
+  //   // checkInvite()
+  //   //   .then((data)=> {console.log('data', data)})
+  //   let userID = user.id;
+  //   const requestOptions = {
+  //     method: 'GET',
+  //     headers: { 'Content-Type': 'application/json' },
+  //   };
+  //   fetch(`/route/checkInvite/${userID}`, requestOptions)
+  //   .then((data)=> {
+  //     data.json()})
+  //   .then((response) => {
+  //     console.log(response)
+
+  //   })
+  //   }, []);
 
   return (
     <div>
